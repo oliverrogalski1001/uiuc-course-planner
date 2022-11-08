ee2c6adc0aaacfe6066d76020836c720213d6143
Oliver Rogalski <33262512+oliverrogalski1001@users.noreply.github.com>
2022-11-07 18:01:05 -0600

    url state

--- src/components/Semester.jsx
+++ src/components/Semester.jsx
@@ -1,12 +1,12 @@
 import AddCourse from "./AddCourse.jsx"
 import Course from "./Course"
 
-const Semester = ({ number, semesterCourses, setCoursePlan, handleDelete }) => {
+const Semester = ({ number, semesterCourses, setCoursePlan, handleDelete, setURL }) => {
 	const totalCreditHours = semesterCourses.reduce((acc, course) => acc + course.creditHours, 0)
 	return (
 		<div className="flex flex-col items-center m-4">
 			<h1 className="text-xl pb-4">Semester: {number}</h1>
-			<AddCourse setCoursePlan={setCoursePlan} semesterIndex={number} />
+			<AddCourse setCoursePlan={setCoursePlan} semesterIndex={number} setURL={setURL} />
 			<h1 className="text-xl py-4">Credit Hours: {totalCreditHours}</h1>
 			<div className="border-0 border-white w-52">
 				<div>
